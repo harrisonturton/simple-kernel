0a06dda9ba4caab85e0c55691aad4b162c760a2c
Harrison Turton <harrisonturton@gmail.com>
2018-08-02 02:46:16 +1000

    Added alternate run command if on Linux

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ echo "Attemping to compile..."
 make
 echo "Finished compiling."
 echo "Running..."
-qemu-system-x86_64 -drive file=main.bin,index=0,media=disk,format=raw
+# qemu-system-x86_64 -drive file=main.bin,index=0,media=disk,format=raw # For OSX
+qemu-system-i386 -drive file=boot.bin,index=0,media=disk,format=raw # For Linux
